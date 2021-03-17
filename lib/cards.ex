fb3185e26e293b0eae66bba6a1e9b54d95fc45bd
Ian <Ian@MacBookPro>
2021-03-17 20:10:27 +0300

    big commit

---
+++ lib/cards.ex
@@ -0,0 +1,30 @@
+defmodule Cards do
+
+  def create_deck do
+
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    List.flatten(
+      for suit <- suits, value <- values do  
+          "#{value} of #{suit}"
+      end)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def save do
+    
+  end
+
+  def load do
+    
+  end
+
+end
